81aec916504dc569a70e840d5ea8cc3bebdf66cc
Surbhi <surbhi.saraogi95@gmail.com>
2018-02-01 10:21:21 +0530

    Removed the print statement from home/views

--- carpool/home/views.py
+++ carpool/home/views.py
@@ -4,6 +4,4 @@ h = Blueprint('home' , __name__ , url_prefix = '/home')
 
 @h.route('/')
 def welcome():
-    print("rendering the welcome page")
-    print("making a change in develop branch")
     return render_template('home/home.html')
